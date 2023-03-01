200609a6ce2bb007175a324108b40f72e9f92919
TheWandererPT <franciscojoseferreiramartins@gmail.com>
2023-03-01 23:08:35 +0000

    still wip

---
+++ Assets/Scripts/Teclas.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teclas : MonoBehaviour
+{
+    public GameObject keypad;
+
+
+
+    private void OnMouseDown()
+    {
+        keypad.GetComponent<Keypad>().RightCombo(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
