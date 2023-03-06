2f19eb92e6fa12bf8bf1b14d580caf6b7c738fa3
TheWandererPT <franciscojoseferreiramartins@gmail.com>
2023-03-06 16:53:30 +0000

    it works

--- Assets/Scripts/Keypad.cs
+++ Assets/Scripts/Keypad.cs
@@ -6,172 +6,17 @@ using TMPro;
 
 public class Keypad : MonoBehaviour
 {
-    //public int[] combination;
-
-    //public GameObject num0;
-    //public GameObject num1;
-    //public GameObject num2;
-    //public GameObject num3;
-    //public GameObject num4;
-    //public GameObject num5;
-    //public GameObject num6;
-    //public GameObject num7;
-    //public GameObject num8;
-    //public GameObject num9;
-
-    //public GameObject clear;
-    //public GameObject enter;
-
-    //public GameObject text;
-
-    //public string wait;
-    //public string denied;
-    //public string granted;
-
-    //List<int> combo = new List<int>();
-    //int index = 0;  
-
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    text.GetComponent<Text>().text = wait;
-    //}
-
-    //public void RightCombo(GameObject recived)
-    //{
-    //    if (recived == num0)
-    //    {
-    //        aumentarCombo(0);
-    //    }
-
-    //    if (recived == num1)
-    //    {
-    //        aumentarCombo(1);
-    //    }
-
-    //    if (recived == num2)
-    //    {
-    //        aumentarCombo(3);
-    //    }
-
-    //    if (recived == num3)
-    //    {
-    //        aumentarCombo(4);
-    //    }
-
-    //    if(recived == num4)
-    //    {
-    //        aumentarCombo(5);
-    //    }
-
-    //    if (recived == num6)
-    //    {
-    //        aumentarCombo(6);
-    //    }
-
-    //    if (recived == num6)
-    //    {
-    //        aumentarCombo(6);
-    //    }
-
-    //    if (recived == num7)
-    //    {
-    //        aumentarCombo(7);
-    //    }
-
-    //    if (recived == num8)
-    //    {
-    //        aumentarCombo(8);
-    //    }
-
-    //    if (recived == num9)
-    //    {
-    //        aumentarCombo(9);
-    //    }
-
-    //    if (recived == clear)
-    //    {
-    //        erase();
-    //    }
-
-    //    if (recived == enter)
-    //    {
-    //        analising();
-    //    }
-    //}
-
-    //void analising()
-    //{
-    //    if (index==combination.Length)
-    //    {
-    //        bool correct = true;
-
-    //        for(int i=0; i<combination.Length; i++)
-    //        {
-    //            if (combo[i] != combination[i])
-    //            {
-    //                correct = false;
-    //                break;
-    //            }
-    //        }
-
-
-    //        if (correct)
-    //        {
-    //            text.GetComponent<Text>().text = granted;
-
-    //        }
-    //        else
-    //        {
-    //            text.GetComponent<Text>().text = denied;
-
-    //        }
-
-
-
-    //    }
-    //    else
-    //    {
-    //        text.GetComponent<Text>().text = denied;
-    //        eraseWithoutChanging();
-    //    }
-    //}
-
-    //void eraseWithoutChanging()
-    //{
-    //    combo.Clear();
-    //    index = 0;
-
-    //}
-
-    //void erase()
-    //{
-    //    combo.Clear();
-    //    index = 0;
-    //    text.GetComponent<Text>().text = wait;
-    //}
-
-
-
-    //public void aumentarCombo(int num)
-    //{
-    //    combo.Add(num); 
-    //    index++;
-    //    text.GetComponent<Text>().text += "";
-
-    //    for (int i = 0; i < index; i++)
-    //    {
-    //        text.GetComponent<Text>().text += "*";
-    //    }
-    //}
-
+    public GameObject input;
     public GameObject keypadObject;
     public Text text;
     public string combination = "0451";
 
+    public GameObject animatedGameObject;
+    public Animator animator;
+    public bool animate;
+
     void Start()
     {
-        //keypadObject.SetActive(false);
 
     }
 
@@ -201,19 +46,21 @@ public class Keypad : MonoBehaviour
     public void Exit()
     {
         keypadObject.SetActive(false);
-
+        //input.GetComponent <InputController> ().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (text.text == "Granted")
+        if (text.text == "Granted" && animate)
         {
+            animator.SetBool("Open", true); 
             Debug.Log("aberto");
         }
 
         if (keypadObject.activeInHierarchy)
         {
+            //input.GetComponent<InputController>().enabled = true;
             Cursor.visible = true;
             Cursor.lockState= CursorLockMode.None;
         }
--- Assets/Scripts/OpenKeypad.cs
+++ Assets/Scripts/OpenKeypad.cs
@@ -9,35 +9,33 @@ public class OpenKeypad : MonoBehaviour
     public Canvas Pegar, Keypad;
     public InputController inputController;
     public Camera MainCamera;
+    public bool reach;
 
     void Start()
     {
-        
+        reach = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        reach=true;
+        Pegar.enabled = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        reach=false;
+        Pegar.enabled = false;
     }
 
     void Update()
     {
-        if(inputController.PegarItem())
+        if(inputController.PegarItem()&&reach)
         {
-            
-            Time.timeScale = 0.0f;
+
+            //Time.timeScale = 0.0f;
             Keypad.enabled = true;
 
         }
-
-       
     }
-
-
-    private void OnTriggerStay(Collider other)
-    {
-        Pegar.enabled = true;
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        Pegar.enabled = false;
-    }
-
-    
 }
